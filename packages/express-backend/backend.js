2a662701a7d026c3737fc35d858b30c73c436273
TylerHBE <tylerhickersonbe@gmail.com>
2026-01-22 22:23:37 -0800

    Created basic REST skeleton

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,7 +1,6 @@
 const express = require('express');
 import cors from "cors";
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const auth = require('../routes/auth.js');
 
 const app = express();
 
@@ -14,27 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/login', (req, res) => {
-  const username = req.body.username;
-  const user = { name: username };
-
-  // authenticate user
-
-  const token = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-});
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ packages/express-backend/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// routes
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+
+  // authenticate user
+
+  const token = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  res.json({ token });
+});
+
+// middleware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = router
+module.exports.authenticateToken = authenticateToken;
